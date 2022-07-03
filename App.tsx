dde4d79a5a3544de9f3c77185d2a2efc5edeb532
GGIEnrike <enrike.churin@gmail.com>
2022-07-03 18:11:29 +0200

    2 files modified

--- App.tsx
+++ App.tsx
@@ -3,9 +3,13 @@ import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div id="app">
+      <div id="sidebar">
+
+      </div>
+      <div id="main">
+
+      </div>
     </div>
   );
 }
