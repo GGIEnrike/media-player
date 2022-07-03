7d4c6357df0f9c6315b22dbd904d653166311bc2
GGIEnrike <enrike.churin@gmail.com>
2022-07-03 18:25:55 +0200

    2 files modified

--- App.tsx
+++ App.tsx
@@ -5,7 +5,10 @@ export default function App() {
   return (
     <div id="app">
       <div id="sidebar">
-
+        <div>Home</div>
+        <div>Library</div>
+        <div>Downloads</div>
+        <div>Settings</div>
       </div>
       <div id="main">
 
