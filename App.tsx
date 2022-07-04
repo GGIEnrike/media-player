6fc5a9de4cdcf7134b258ef577522bc2705541bb
GGIEnrike <enrike.churin@gmail.com>
2022-07-04 16:22:53 +0200

    1 file modified

--- App.tsx
+++ App.tsx
@@ -18,7 +18,25 @@ function Home() {
 function Library() {
   return (
     <div>
-      <h1 className="Header"></h1>
+      <h1 className="Header">Library</h1>
+      <hr />
+    </div>
+  )
+}
+
+function Downloads() {
+  return (
+    <div>
+      <h1 className="Header">Downloads</h1>
+      <hr />
+    </div>
+  )
+}
+
+function Settings() {
+  return (
+    <div>
+      <h1 className="Settings">Settings</h1>
       <hr />
     </div>
   )
@@ -29,13 +47,14 @@ export default function App() {
     <div id="app">
       <nav id="sidebar">
         <Link to="/home">Home</Link>
-        <Link to="Library">Library</Link>
-        <Link to="Downloads">Downloads</Link>
-        <Link to="Settings">Settings</Link>
+        <Link to="/library">Library</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/settings">Settings</Link>
       </nav>
       <div id="main">
         <Routes>
           <Route path="home" element={<Home />} />
+          <Route path="library" element={<Library />} />
         </Routes>
       </div>
     </div>
