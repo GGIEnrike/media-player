51a0132adb4fe7ce2f87f93000106f70dec2e21b
GGIEnrike <enrike.churin@gmail.com>
2022-07-04 18:29:59 +0200

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,64 +1,64 @@
 import * as React from 'react';
-import {
-  Link,
-  Routes,
-  Route
-} from "react-router-dom";
+import { Link, Routes, Route } from 'react-router-dom';
 import './style.css';
 
 function Card() {
   return (
-    <div className="Card">
-      <h4>Film Name</h4>
+    <Link to="/library/doctor-strange" className="Card">
+      <h4>Dr. Strange</h4>
       <img src="https://static.hdrezka.ac/i/2022/2/18/j09948c99c8cfwk71c80m.png"></img>
-    </div>
-  )
+    </Link>
+  );
 }
 
 function CardView() {
   return (
     <div className="CardView">
-      { Array.apply(null, Array(25)).map(function (x, i) { return <Card /> }) }
+      {Array.apply(null, Array(4)).map(function (x, i) {
+        return <Card />;
+      })}
     </div>
-  )
+  );
 }
 
 function Home() {
   return (
-    <div className="content">
+    <div className="content">
       <h1>Home</h1>
       <hr />
       <h2>Recommended for you</h2>
-      <CardView category="Recommended"/>
+      <CardView category="Recommended" />
+      <h2>Continue watching</h2>
+      <CardView category="Continue" />
     </div>
-  )
+  );
 }
 
 function Library() {
   return (
-    <div className="content">
-      <h1 >Library</h1>
+    <div className="content">
+      <h1>Library</h1>
       <hr />
     </div>
-  )
+  );
 }
 
 function Downloads() {
   return (
-    <div className="content">
-      <h1 >Downloads</h1>
+    <div className="content">
+      <h1>Downloads</h1>
       <hr />
     </div>
-  )
+  );
 }
 
 function Settings() {
   return (
-    <div className="content">
-      <h1 >Settings</h1>
+    <div className="content">
+      <h1>Settings</h1>
       <hr />
     </div>
-  )
+  );
 }
 
 export default function App() {
